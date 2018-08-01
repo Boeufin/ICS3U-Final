aad79ee18f2ce9d4c01098d85198dccee4f4ed2b
1801-AD-GQWW5M2\outreach2 <ethan.szeto@gmail.com>
2018-08-01 10:33:10 -0400

    Fixed the edit and complete button position.

--- project1/app/scripts/createTask.js
+++ project1/app/scripts/createTask.js
@@ -4,7 +4,6 @@ var taskDetails = document.getElementById("newDetails");
 var addButton=document.getElementsByTagName("button")[0];
 var incompletetaskList=document.getElementById("incomplete-tasks");
 
-
 //Takes the two inputs and translates them to taskString and detailsInput within this function.
 function createNewTaskElement(taskString, detailsInput)
 {
@@ -32,63 +31,68 @@ function createNewTaskElement(taskString, detailsInput)
   labeldesc.innerText = detailsInput;
   editTextArea.type = "textarea";
   editTextArea.style.resize = "none";
+  listItem.style.top = "-50px";
+
+  //Task Title position
+  label.style.position = "relative";
+  label.style.top = "-80px";
 
   //Edit Button CSS
   editButton.style.position = "relative";
-  editButton.style.left = "235px";
-  editButton.style.top = "-50px";
+  editButton.style.left = "225px";
+  editButton.style.top = "-15px";
 
   //Complete Button CSS
   completeButton.style.position = "relative";
-  completeButton.style.left = "150px";
-  completeButton.style.top = "0px";
+  completeButton.style.left = "140px";
+  completeButton.style.top = "35px";
+
+  //Details positioning
+  labeldesc.style.position = "relative";
+  labeldesc.style.top = "-60px"
 
   //Hiding the text area element Initially
   editTextArea.style.display = "none";
   
   //Adding the button classes and names inside the added task.
-  editButton.innerText="Edit";
-  editButton.className="edit";
-  completeButton.innerText="Complete";
-  completeButton.className="complete";
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  completeButton.innerText = "Complete";
+  completeButton.className = "complete";
 
   //appending the created HTML elements to the HTML page.
-  listItem.appendChild(label);
-  listItem.appendChild(lbreak);
-  listItem.appendChild(labeldesc);
-  listItem.appendChild(tbreak);
   listItem.appendChild(editButton);
   listItem.appendChild(completeButton);
+  listItem.appendChild(label);
+  listItem.appendChild(lbreak);
   listItem.appendChild(editTextArea);
+  listItem.appendChild(tbreak);
+  listItem.appendChild(labeldesc);
   listItem.appendChild(separater);
   return listItem;
 }
 
-
 //Completes the task.
 function CompleteTask() 
-{
-    
-  var listItem=this.parentNode;
-  var ul=listItem.parentNode;
+{  
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
   ul.removeChild(listItem);
 }
 
-//Gives functionality to the edit and complete buttons.
+//Binding functionality to the edit and complete buttons.
 function bindTaskEvents(taskListItem)
 {
-  
-  var editButton=taskListItem.querySelector("button.edit");
-  var completeButton=taskListItem.querySelector("button.complete");
+  var editButton = taskListItem.querySelector("button.edit");
+  var completeButton = taskListItem.querySelector("button.complete");
 
-  editButton.onclick=editTask;
-  completeButton.onclick=CompleteTask;
+  editButton.onclick = editTask;
+  completeButton.onclick = CompleteTask;
 }
 
 //addTask function calls the createTask function above.
 function addTask()
 {
-  debugger;
   var listItem = createNewTaskElement(taskTitle.value, taskDetails.value);
   //Adding the listed items to the incompleteTaskList.
   incompletetaskList.appendChild(listItem);
@@ -96,19 +100,19 @@ function addTask()
   bindTaskEvents(listItem);
 
  //Resets the values of the input field.
-  taskTitle.value="";
-  taskDetails.value="";
+  taskTitle.value = "";
+  taskDetails.value = "";
 }
 
 //Edit the existing text area.
 function editTask()
 {
-
-  var listItem = this.parentNode;//Targets the part of the list that the button was clicked by.
+  var listItem = this.parentNode;//Targets the button that was just clicked.
   var editInput = listItem.querySelector('textarea');
   var label = listItem.querySelector("label");
   var detailDesc = listItem.querySelector("textArea");
   var editTaskButton = listItem.querySelector("button");
+
   //Hides and displays the text area for edits.
   if (detailDesc.style.display == "none") {
     detailDesc.style.display = "block";
@@ -117,13 +121,15 @@ function editTask()
   }
 
   //turns the class to editmode.
-  var containsClass=listItem.classList.contains("editMode");
-    if(containsClass){
-      label.innerText=editInput.value;
-    } else {
-      editInput.value=label.innerText;
-    }
+  var containsClass = listItem.classList.contains("editMode");
 
+  if(containsClass) {
+    label.innerText = editInput.value;
+  } else {
+    editInput.value = label.innerText;
+  }
+
+  //Changes the edit button to a save button.
   if (detailDesc.style.display == "none") {
     editTaskButton.innerHTML = "Edit";
   } else {
@@ -133,7 +139,8 @@ function editTask()
   listItem.classList.toggle("editMode");
 }
 
+//Sets the task holder to nothing.
 function clearAll()
- {
-document.getElementById("incomplete-tasks").innerText = "";
+{
+  document.getElementById("incomplete-tasks").innerText = "";
 }
